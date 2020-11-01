dc3b3039239394e0c2b730d3820181de6e32dc3e
pavleciric89 <pavleciric89@gmail.com>
2020-11-01 03:47:36 +0100

    added more style

--- src/components/BookList/Card.js
+++ src/components/BookList/Card.js
@@ -8,7 +8,10 @@ const Card = (props) => {
       <h3 className="title">{title}</h3>
       <p className="author">{author}</p>
       <i className="bx bx-x bx-md" onClick={() => deleteHandler({ id })}></i>
-      <p className="price">{price}&#8364;</p>
+      <div className="price-cart">
+        <p className="price">{price}&#8364;</p>
+        <i class="bx bxs-cart-add bx-sm" title="Add to cart"></i>
+      </div>
     </article>
   );
 };
