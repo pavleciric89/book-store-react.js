34429c387353258a6e63dd32d3a1b6524505a9a5
Dan Radenkovic <bogdan.radenkovic@gmail.com>
2020-11-05 12:42:25 +0100

    Project setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from "react";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import CardList from "./components/BookList/CardList";
 import Navbar from "./components/Navbar/Navbar";
-import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import Searchbar from "./components/Searchbar/Searchbar";
 import Main from "./components/LandingPage/Main";
+
 function App() {
   return (
     <Router>
