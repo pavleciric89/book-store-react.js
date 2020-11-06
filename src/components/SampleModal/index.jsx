1873c4c9347b640e6eceb372222a998bd4a4ef28
Dan Radenkovic <bogdan.radenkovic@gmail.com>
2020-11-06 19:31:54 +0100

    Cleanup modal

--- src/components/SampleModal/index.jsx
+++ src/components/SampleModal/index.jsx
@@ -2,18 +2,25 @@ import React from "react";
 import "./style.css";
 
 export default function SampleModal({ close, book }) {
-  
+  if (!book) return null;
   return (
-    <div className="modal-overlay">
-      <div className="modal-body">
-        <button onClick={close}>&times;</button>
-        <br/>
-        Content
-        <div>
-          {book.title}
+    <div className="modal-wrapper">
+        <div className="modal-backdrop" />
+        <div className="modal-box">
+          <i onClick={close} className="bx bx-x bx-md close-modal"></i>
+
+          <div className="flex-modal">
+            <div className="img-modal-container">
+              <img className="img-modal" src={book.img} alt="img" />
+            </div>
+            <div className="text-modal">
+              <h3 className="title-modal">{book.title}</h3>
+              <p className="author-modal">{book.author}</p>
+              <p className="details-title-modal">Details:</p>
+              <p className="details-modal">{book.details}</p>
+            </div>
+          </div>
         </div>
       </div>
-    </div>,
-    document.getElementById("modal-root")
   );
 }
